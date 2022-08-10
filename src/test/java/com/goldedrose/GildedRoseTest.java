628ad51eb2bcb45b705da4c6eac638918bad7638
SepehrNamdar <sepehr_gsfc@yahoo.com>
2022-08-10 19:16:21 +0200

    Jalase Avval

--- src/test/java/com/goldedrose/GildedRoseTest.java
+++ src/test/java/com/goldedrose/GildedRoseTest.java
@@ -2,15 +2,26 @@ package com.goldedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.approvaltests.combinations.CombinationApprovals.verifyAllCombinations;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void should_update_quality() {
+        String[] name =
+                new String[] {null, "", "foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
+        Integer[] sellIn = new Integer[] {null, 0, -1, 10, 11, 12, 13, 14};
+        Integer[] quality = new Integer[] {null, 0, 1, 50, 49, 51};
+
+        verifyAllCombinations(this::verifyUpdateQuality, name, sellIn, quality);
+    }
+
+    private String verifyUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+
+        return app.items[0].toString();
     }
 }
